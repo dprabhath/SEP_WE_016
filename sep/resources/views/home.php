3a4b126ba0505707993b8d562b7704a31b7b6acb
AltairCA <alexvista1234@gmail.com>
2016-01-21 23:41:07 +0530

    Home Page/Template
    Add a basic Homepage, and the basic template, you can find them under
    app\resources\views\template\ directory.and home.php shows how to use
    the template

---
+++ sep/resources/views/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php
+	include 'template/head.php';
+	?>
+	<link rel="stylesheet" href="resources/home/css/video.css">
+	
+</head>
+<?php
+	include 'template/body.php';
+	?>
+ <body class="bodybg">
+    	<video autoplay loop poster="resources/home/picture/Big-City-Life.jpg" id="bgvid">
+    <source src="resources/home/video/Big-City-Life.mp4" type="video/mp4"></source>
+</video>
+
+	<div class="container-fluid">
+		
+	</div>
+</body>
+</html>
---
+++ sep/resources/views/template/head.php
@@ -0,0 +1,21 @@
+<meta charset="utf-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="resources/common/css/bootstrap.css">
+
+<!-- jQuery library -->
+
+
+<!-- Latest compiled JavaScript -->
+
+<script src="resources/common/js/jquery-2.2.0.js"></script>
+
+<script src="resources/common/js/bootstrap.js"></script>
+
+<style type="text/css">
+	body{
+		min-height:100%;
+		min-width: 100%;
+		height: 100%;
+	}
+
+</style>
