fc07110a7bef557d9d0d255db443fa507bf5aa55
diharaw <shredhead94@gmail.com>
2016-01-24 00:35:20 +0800

    Update DoctorList with Background

--- sep/resources/views/DoctorList.php
+++ sep/resources/views/DoctorList.php
@@ -4,23 +4,143 @@
 	<?php
 	include 'template/head.php';
 	?>
-	<link rel="stylesheet" href="resources/home/css/video.css">
+	<style type="text/css">
+
+		body{
+			background-image: url("resources/common/picutres/bg.jpg");
+			color:white;
+			
+		}
+	</style>
 	
 </head>
-<?php include 'template/body.php';?>
- 	<body class="bodybg">
-    	<video autoplay loop poster="resources/home/picture/Big-City-Life.jpg" id="bgvid">
-    		<source src="resources/home/video/Big-City-Life.mp4" type="video/mp4"></source>
-		</video>
 
-		<div class="container-fluid">
+ 	<body>
+ 	<?php include 'template/body.php';?>
+		<div class="container-fluid" style="margin-left:25%;" >
 		
-			<div class="row">
-        		<div class="col-lg-1"> <h1>ONE </h1> </div>
-        		<div class="col-lg-10"> <h1> TWO</h1> </div>
+			<div class="row" >
+        		<div class="col-lg-2" style="background-color:rgba(0,0,0,0.5);padding-left:30px;"> <h2> Filters </h2> </div>
+        		<div class="col-lg-6" style="background-color:rgba(0,0,0,0.5);margin-left:10px;"> <h2> Doctors </h2> </div>
     		</div>
 
-		
+    		<div class="row">
+
+        		<div class="col-lg-2" style="background-color:rgba(0,0,0,0.5);padding-left:30px;margin-top:10px;margin-bottom:10px;height:100vh;"> 
+
+        			<h4> By Location </h4> 
+        			<ul style="list-style-type:disc">
+  						<li><a style="color:white">Location1</a></li>
+  						<li><a style="color:white">Location2</a></li>
+  						<li><a style="color:white">Location3</a></li>
+  						<li><a style="color:white">Location4</a></li>
+  						<li><a style="color:white">Location5</a></li>
+  						<li><a style="color:white">Location6</a></li>
+  						<li><a style="color:white">Location7</a></li>
+  						<li><a style="color:white">Location8</a></li>
+  						<li><a style="color:white">Location9</a></li>
+					</ul>
+
+					<h4> By Specialization </h4> 
+        			<ul style="list-style-type:disc">
+  						<li><a style="color:white">Spec1</a></li>
+  						<li><a style="color:white">Spec2</a></li>
+  						<li><a style="color:white">Spec3</a></li>
+					</ul>
+
+        		</div>
+
+        		<div class="col-lg-6" style="background-color:rgba(0,0,0,0.5);margin-left:10px;margin-top:10px;margin-bottom:10px;height:100vh;">
+        			<div class="row">
+        				<div class="col-lg-10"  style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;"> 
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					<a style="padding-left:15px;color:white"> Jeff Loomis </a> 
+        					
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10" style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;"> 
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					 <a  style="padding-left:15px;color:white"> Mike Mangini </a> 
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10" style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;"> 
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					<a style="padding-left:15px;color:white"> John Petrucci </a>
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10" style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;"> 
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					<a style="padding-left:15px;color:white"> Dave Mustaine </a> 
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10" style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;"> 
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					<a style="padding-left:15px;color:white"> John Hable </a> 
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10" style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;"> 
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					<a style="padding-left:15px;color:white"> Wolfgang Engle </a> 
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10" style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;">
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					<a style="padding-left:15px;color:white"> John Carmack </a> 
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10"  style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;"> 
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					<a style="padding-left:15px;color:white"> Jeff Loomis </a> 
+        					
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10" style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;"> 
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					 <a  style="padding-left:15px;color:white"> Mike Mangini </a> 
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10" style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;"> 
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					<a style="padding-left:15px;color:white"> John Petrucci </a>
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10" style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;"> 
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					<a style="padding-left:15px;color:white"> Dave Mustaine </a> 
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10" style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;"> 
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					<a style="padding-left:15px;color:white"> John Hable </a> 
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10" style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;"> 
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					<a style="padding-left:15px;color:white"> Wolfgang Engle </a> 
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col-lg-10" style="border-style: solid;border-width: 1px;border-color:black;padding-top:10px;padding-bottom:10px;">
+        					<img src="resources/common/picutres/background.jpg" height="42" width="42" alt="..." class="img-rounded"> 
+        					<a style="padding-left:15px;color:white"> John Carmack </a> 
+        			</div>
+        			</div>
+        			
+        		</div>
+    		</div>
+
 
 		</div>
 	</body>
